85ee44c96cc3280b6cfd1e2ae371a84d93eb9d37
George Cantwell <41652900+gcant@users.noreply.github.com>
2019-10-10 12:27:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ H = np.array(H)
 P_MC = np.array([np.mean(H==i,axis=0) for i in range(G.number_of_nodes())])
 
 P_exact = full_marginal.one_node_marginals(G)
-P_exact /= np.sum(P_exact[0])
 
 mean_MC = np.dot(P_MC, np.arange(G.number_of_nodes()))
 mean_exact = np.dot(P_exact, np.arange(G.number_of_nodes()))
